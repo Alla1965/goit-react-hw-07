29b22e94cd33980d66a41f4a094916ec43f3ae85
Alla1965 <savanna.opt@gmail.com>
2025-05-02 19:16:50 +0300

    fix filter

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,14 +5,8 @@ import { Contact } from '../Contact/Contact';
 import css from './ContactList.module.css';
 
 export const ContactList = () => {
-// console.log(contacts);
 
-  const contacts = useSelector(selectFilteredContacts);
-  const filter = useSelector(state => state.filters.name);
-  
- const visibleContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const visibleContacts  = useSelector(selectFilteredContacts);
 
   return (
     <ul className={css.list}>
